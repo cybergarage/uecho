42c23bf22879c8cc8e54acf2287f95fb06b0e080
Satoshi Konno <skonno@cybergarage.org>
2015-08-02 21:09:19 +0900

    * Updated uecho_message_?ettid().

--- tests/MessageTest.cpp
+++ tests/MessageTest.cpp
@@ -14,13 +14,19 @@
 
 BOOST_AUTO_TEST_CASE(MessageTid)
 {
+  int tid;
+  
   uEchoMessage *msg = uecho_message_new();
   
   for (int n=0; n<100; n++) {
     BOOST_CHECK(uecho_message_settid(msg, n));
-    int tid = uecho_message_gettid(msg);
+    tid = uecho_message_gettid(msg);
     BOOST_CHECK_EQUAL(n, tid);
   }
+  
+  BOOST_CHECK(uecho_message_settid(msg, UECHO_TID_MAX));
+  tid = uecho_message_gettid(msg);
+  BOOST_CHECK_EQUAL(tid, UECHO_TID_MAX);
   
   uecho_message_delete(msg);
 }
