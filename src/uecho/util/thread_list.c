a01d8679ab363e84fdd0ae951147fa8e392b45b7
Satoshi Konno <skonno@cybergarage.org>
2015-08-05 21:28:18 +0900

    * Imported thread functions from mUPnP for C.

---
+++ src/uecho/util/thread_list.c
@@ -0,0 +1,69 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <uecho/util/thread.h>
+
+/****************************************
+* uecho_threadlist_new
+****************************************/
+
+uEchoThreadList *uecho_threadlist_new()
+{
+	uEchoThreadList *threadList;
+
+	threadList = (uEchoThreadList *)malloc(sizeof(uEchoThreadList));
+
+	if ( NULL != threadList ) {
+		uecho_list_header_init((uEchoList *)threadList);
+		threadList->runnableFlag = false;
+		threadList->action = NULL;
+		threadList->userData = NULL;
+	}
+
+	return threadList;
+}
+
+/****************************************
+* uecho_threadlist_delete
+****************************************/
+
+void uecho_threadlist_delete(uEchoThreadList *threadList)
+{
+	uecho_threadlist_clear(threadList);
+	free(threadList);
+}
+
+/****************************************
+* uecho_threadlist_start
+****************************************/
+
+bool uecho_threadlist_start(uEchoThreadList *threadList)
+{
+	uEchoThreadList *thread;
+
+	for (thread = uecho_threadlist_gets(threadList); thread != NULL; thread = uecho_thread_next(thread))
+		uecho_thread_start(thread);
+
+	return true;
+}
+
+/****************************************
+* uecho_threadlist_stop
+****************************************/
+
+bool uecho_threadlist_stop(uEchoThreadList *threadList)
+{
+	uEchoThreadList *thread;
+	
+	for (thread = uecho_threadlist_gets(threadList); thread != NULL; thread = uecho_thread_next(thread))
+		uecho_thread_stop(thread);
+
+	return true;
+}
