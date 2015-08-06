1219b2ee01ff07393ce206ea6d284d7db37673a9
Satoshi Konno <skonno@cybergarage.org>
2015-08-06 19:30:07 +0900

    * Added test cases.

--- src/uecho/util/mutex.c
+++ src/uecho/util/mutex.c
@@ -22,34 +22,34 @@ uEchoMutex *uecho_mutex_new()
 {
 	uEchoMutex *mutex;
 
-    mutex = (uEchoMutex *)malloc(sizeof(uEchoMutex));
+  mutex = (uEchoMutex *)malloc(sizeof(uEchoMutex));
 
-	if ( NULL != mutex )
-	{
+  if (!mutex)
+    return NULL;
+
 #if defined(WIN32) && !defined(ITRON)
-		mutex->mutexID = CreateMutex(NULL, false, NULL);
+  mutex->mutexID = CreateMutex(NULL, false, NULL);
 #elif defined(BTRON)
-		mutex->mutexID = cre_sem(1, SEM_EXCL);
+  mutex->mutexID = cre_sem(1, SEM_EXCL);
 #elif defined(ITRON) 
-		T_CSEM	csem;
-		csem.sematr = TA_TFIFO;
-		csem.isemcnt = 1;
-		csem.maxsem = 1;
-		csem.name = NULL;
-		mutex->mutexID = acre_sem(&csem);
+  T_CSEM	csem;
+  csem.sematr = TA_TFIFO;
+  csem.isemcnt = 1;
+  csem.maxsem = 1;
+  csem.name = NULL;
+  mutex->mutexID = acre_sem(&csem);
 #elif defined(TENGINE) && !defined(PROCESS_BASE)
-		T_CSEM	csem;
-		csem.exinf = 0;
-		csem.sematr = TA_TFIFO | TA_FIRST;
-		csem.isemcnt = 0;
-		csem.maxsem = 1;
-		mutex->mutexID = tk_cre_sem(&csem);
+  T_CSEM	csem;
+  csem.exinf = 0;
+  csem.sematr = TA_TFIFO | TA_FIRST;
+  csem.isemcnt = 0;
+  csem.maxsem = 1;
+  mutex->mutexID = tk_cre_sem(&csem);
 #elif defined(TENGINE) && defined(PROCESS_BASE)
-		mutex->mutexID = b_cre_sem(1, SEM_EXCL);
+  mutex->mutexID = b_cre_sem(1, SEM_EXCL);
 #else
-		pthread_mutex_init(&mutex->mutexID, NULL);
+  pthread_mutex_init(&mutex->mutexID, NULL);
 #endif
-	}
 
 	return mutex;
 }
@@ -84,200 +84,7 @@ bool uecho_mutex_delete(uEchoMutex *mutex)
 /****************************************
 * uecho_mutex_lock
 ****************************************/
-#if defined(WITH_THREAD_LOCK_TRACE) && defined(__USE_ISOC99)
-#include <string.h>
-#include <pthread.h>
 
-/* Contains record for every thread which has lock or is
- * waiting for lock
- */
-uEchoLockInfo *uecho_tlt_list = NULL;
-
-/* Used to synchronize the thread lock record db access.
- */
-pthread_mutex_t tlt_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-bool uecho_mutex_lock_trace(	const char *file, 
-				int line, 
-				const char *function,
-				uEchoMutex *mutex)
-{
-	pthread_t thid;
-	int found;
-	uEchoLockInfo *temp;
-
-	if (!mutex)
-		return false;
-
-	thid = pthread_self();
-	pthread_mutex_lock(&tlt_mutex);
-
-	found = 0;
-
-	/* Searching existing lock record for this thread.
-	 */
-	temp = uecho_tlt_list;
-	while ( NULL != temp ) {
-		if ( pthread_equal(temp->thread_id, thid) && ( temp->mutex_id == (int)mutex ) ) {
-			found = 1;
-			break;
-		}
-		temp = temp->next;
-	}
-
-	if (found)
-	{
-		/* Double locking detected */
-		FILE *error_dump = NULL;
-
-		system("date >>/tmp/uecho_td_errors.log");
-		error_dump = fopen("/tmp/uecho_td_errors.log", "a");
-		fprintf(stderr, "Double locking detected, locked from\n\tfile: %s\n\tline: %d\n\tfunction: %s\ntried to lock again from\n\tfile: %s\n\tline: %d\n\tfunction: %s\nfailing fast...\n",
-                        temp->file,
-                        temp->line,
-                        temp->function,
-                        file,
-                        line,
-                        function);
-                fprintf(error_dump, "Double locking detected, locked from\n\tfile: %s\n\tline: %d\n\tfunction: %s\ntried to lock again from\n\tfile: %s\n\tline: %d\n\tfunction: %s\nfailing fast...\n",
-                        temp->file,
-                        temp->line,
-                        temp->function,
-                        file,
-                        line,
-                        function);
-                fclose(error_dump);
-                exit(-1);
-        }
-
-	/* No locks found for this thread, inserting new record.
-	 */
-
-	temp = malloc(sizeof(uEchoLockInfo));
-	temp->thread_id = thid;
-        temp->file = strdup(file);
-        temp->line = line;
-        temp->function = strdup(function);
-	/* Using the memory address for differentiating between different
-	 * mutexes... */
-	temp->mutex_id = (int)mutex;
-
-        temp->next = uecho_tlt_list;
-	uecho_tlt_list = temp;
-
-	pthread_mutex_unlock(&tlt_mutex);
-
-	if ( EBUSY == pthread_mutex_trylock(&mutex->mutexID) )
-	{
-		/* Lock already held by someone, printing out information
-		 * about thread having lock and threads waiting for lock.
-		 */
-		pthread_mutex_lock(&tlt_mutex);
-		temp = uecho_tlt_list;
-
-		while ( NULL != temp  && ( temp->mutex_id == (int)mutex ) ) {
-			FILE *error_dump = NULL;
-
-			system("date >>/tmp/uecho_td_errors.log");
-			error_dump = fopen("/tmp/uecho_td_errors.log", "a");
-			fprintf(stderr, "Already locked from\n\tfile: %s\n\tline: %d\n\tfunction: %s\ntried to lock from\n\tfile: %s\n\tline: %d\n\tfunction: %s\ngoing to be blocked for a while...\n",
-				temp->file,
-				temp->line,
-				temp->function,
-				file,
-				line,
-				function);
-			fprintf(error_dump, "Already locked from\n\tfile: %s\n\tline: %d\n\tfunction: %s\ntried to lock from\n\tfile: %s\n\tline: %d\n\tfunction: %s\ngoing to be blocked for a while...\n",
-				temp->file,
-				temp->line,
-				temp->function,
-				file,
-				line,
-				function);
-			fclose(error_dump);
-
-			temp = temp->next;
-		}
-
-		pthread_mutex_unlock(&tlt_mutex);
-
-		/* Actual locking */
-		pthread_mutex_lock(&mutex->mutexID);
-	}
-
-	return true;
-}
-
-bool uecho_mutex_unlock_trace(	const char *file, 
-				int line, 
-				const char *function,
-				uEchoMutex *mutex)
-{
-        pthread_t thid;
-        int found;
-        uEchoLockInfo *temp, *ptemp;
-
-		if (!mutex)
-			return false;
-
-        thid = pthread_self();
-        pthread_mutex_lock(&tlt_mutex);
-
-        found = 0;
-
-        temp = uecho_tlt_list;
-        ptemp = NULL;
-        while ( NULL != temp ) {
-                if ( pthread_equal(temp->thread_id, thid)  && ( temp->mutex_id == (int)mutex ) ) {
-                        found = 1;
-                        break;
-                }
-                ptemp = temp;
-                temp = temp->next;
-        }
-
-        /* Removing lock record from list
-         */
-        if (found && ( NULL != ptemp ) )
-        {
-                ptemp->next = temp->next;
-                free(temp->file);
-                free(temp->function);
-                free(temp);
-                temp = NULL;
-        }
-        /* First list record handled as a special case
-         */
-        else if ( found && ( NULL == ptemp ) )
-        {
-                uecho_tlt_list = temp->next;
-                free(temp->file);
-                free(temp->function);
-                free(temp);
-        }
-        else
-        {
-                /* Double unlock */
-                FILE *error_dump = NULL;
-
-                system("date >>/tmp/uecho_td_errors.log");
-                error_dump = fopen("/tmp/uecho_td_errors.log", "a");
-
-                fprintf(stderr, "Unlocking from %s, (f:%s l:%d), but cannot find lock record...\n",
-                        function, file, line);
-                fprintf(error_dump, "Unlocking from %s, (f:%s l:%d), but cannot find lock record...\n",
-                        function, file, line);
-                fclose(error_dump);
-                exit(-1);
-        }
-
-        pthread_mutex_unlock(&tlt_mutex);
-
-	pthread_mutex_unlock(&mutex->mutexID);
-
-	return true;
-}
-#else
 bool uecho_mutex_lock(uEchoMutex *mutex)
 {
 	if (!mutex)
@@ -324,4 +131,3 @@ bool uecho_mutex_unlock(uEchoMutex *mutex)
 #endif
 	return true;
 }
-#endif /* WITH_THREAD_LOCK_TRACE */
---
+++ tests/ThreadTest.cpp
@@ -0,0 +1,40 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+
+#include <uecho/util/thread.h>
+
+const int THREAD_TEST_LOOP_NUM = 10;
+
+void TestThereadFunc(uEchoThread *thread) {
+  int *testCounter = (int *)uecho_thread_getuserdata(thread);
+  for (int n = 0; n < THREAD_TEST_LOOP_NUM; n++) {
+    (*testCounter)++;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(ThreadTest) {
+  uEchoThread *thread = uecho_thread_new();
+  
+  int testCounter = 0;
+  uecho_thread_setaction(thread, TestThereadFunc);
+  uecho_thread_setuserdata(thread, &testCounter);
+  
+  BOOST_CHECK_EQUAL (uecho_thread_start(thread), true);
+  while (testCounter != THREAD_TEST_LOOP_NUM) {
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+  }
+  BOOST_CHECK_EQUAL(testCounter, THREAD_TEST_LOOP_NUM);
+  BOOST_CHECK_EQUAL(uecho_thread_stop(thread), true);
+  
+  uecho_thread_delete(thread);
+}
