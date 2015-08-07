42c6773c2452e2ccd88bf02dd3e9c2cba5fd9f55
Satoshi Konno <skonno@cybergarage.org>
2015-08-08 02:10:13 +0900

    * Added server list functions.

---
+++ src/uecho/core/udp_server_list.c
@@ -0,0 +1,9 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
