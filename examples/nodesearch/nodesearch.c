f558d8b46a268dc3ba892f3aec0a9c3c23105ba7
Satoshi Konno <skonno@cybergarage.org>
2015-08-12 20:57:22 +0900

    * Added examples.

---
+++ examples/nodesearch/nodesearch.c
@@ -0,0 +1,17 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <uecho/controller.h>
+
+int main(int argc, char *argv[])
+{
+  return EXIT_SUCCESS;
+}
+
