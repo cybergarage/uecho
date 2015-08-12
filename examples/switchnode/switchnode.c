ff52b941d5bb4172ddc176a083dcf04291188eef
Satoshi Konno <skonno@cybergarage.org>
2015-08-12 20:57:22 +0900

    * Added examples.

---
+++ examples/switchnode/switchnode.c
@@ -0,0 +1,17 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <uecho/node.h>
+
+int main(int argc, char *argv[])
+{
+  return EXIT_SUCCESS;
+}
+
