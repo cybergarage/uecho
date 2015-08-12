c79535fcd664aed6820aeceeebe5e5c4f3a69c71
Satoshi Konno <skonno@cybergarage.org>
2015-08-12 21:10:33 +0900

    * Updated configure files on MacOSX 10.10.

--- tests/NodeTest.cpp
+++ tests/NodeTest.cpp
@@ -9,7 +9,12 @@
  ******************************************************************/
 
 #include <boost/test/unit_test.hpp>
+#include <uecho/node.h>
 
 BOOST_AUTO_TEST_CASE(Node)
 {
+  uEchoNode *node = uecho_node_new();
+  BOOST_CHECK(node);
+  
+  uecho_node_delete(node);
 }
