f2b296e04ab017b564b160b656a07349c0a5616b
Satoshi Konno <skonno@cybergarage.org>
2015-08-20 20:51:10 +0900

    * Updated object functions.

--- tests/ProfileTest.cpp
+++ tests/ProfileTest.cpp
@@ -23,3 +23,20 @@ BOOST_AUTO_TEST_CASE(ProfileSuperClassMandatoryProperties)
   
   uecho_object_delete(obj);
 }
+
+BOOST_AUTO_TEST_CASE(NodeProfileObjectMandatoryProperties)
+{
+  uEchoObject *obj = uecho_nodeprofileclass_new();
+
+  BOOST_CHECK(uecho_object_iscode(obj, uEchoNodeProfileObject));
+
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileManufacturerCode));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileAnnoPropertyMap));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileSetPropertyMap));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileGetPropertyMap));
+
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoNodeProfileClassOperatingStatus));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoNodeProfileClassVersionInformation));
+  
+  uecho_object_delete(obj);
+}
