3076e703ddf7924e652e6d1b3235c110e76875cf
Satoshi Konno <skonno@cybergarage.org>
2015-08-19 17:51:51 -1000

    * Updated profile functions.

---
+++ tests/ProfileTest.cpp
@@ -0,0 +1,25 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include <uecho/profile.h>
+
+BOOST_AUTO_TEST_CASE(ProfileSuperClassProperties)
+{
+  uEchoObject *obj = uecho_profile_new();
+  
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileSuperClassManufacturerCode));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileSuperClassAnnoPropertyMap));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileSuperClassSetPropertyMap));
+  BOOST_CHECK(uecho_object_hasproperty(obj, uEchoProfileSuperClassGetPropertyMap));
+  
+  uecho_object_delete(obj);
+}
