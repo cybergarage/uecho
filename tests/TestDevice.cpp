211fcd27d98d2761505619b5a8da0ada87d028d2
Satoshi Konno <skonno@cybergarage.org>
2015-08-27 13:00:09 +0900

    * Updated search functions.

--- tests/TestDevice.cpp
+++ tests/TestDevice.cpp
@@ -13,5 +13,14 @@
 uEchoObject *uecho_test_createtestdevice()
 {
   uEchoObject *dev = uecho_device_new();
+  uecho_object_setmanufacturercode(dev, UECHO_TEST_MANCODE);
+  uecho_object_setcode(dev, UECHO_TEST_OBJECTCODE);
   return dev;
+}
+
+uEchoNode *uecho_test_createtestnode() {
+  uEchoNode *node = uecho_node_new();
+  uEchoObject *dev = uecho_test_createtestdevice();
+  uecho_node_addobject(node, dev);
+  return node;
 }
--- tests/TestDevice.h
+++ tests/TestDevice.h
@@ -11,8 +11,13 @@
 #ifndef _UECHO_TEST_TESTDEVICE_H_
 #define _UECHO_TEST_TESTDEVICE_H_
 
+#include <uecho/node.h>
 #include <uecho/device.h>
 
+const uEchoObjectCode UECHO_TEST_MANCODE = 0x000000;
+const uEchoObjectCode UECHO_TEST_OBJECTCODE = 0xF00101;
+
+uEchoNode *uecho_test_createtestnode();
 uEchoObject *uecho_test_createtestdevice();
 
 #endif /* _UECHO_TEST_TESTDEVICE_H_ */
