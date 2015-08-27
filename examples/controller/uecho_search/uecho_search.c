2f1da443ba402f8b4a23014d57abfcccc87f5e64
Satoshi Konno <skonno@cybergarage.org>
2015-08-27 22:41:20 +0900

    * Updated examples.

--- examples/controller/uecho_search/uecho_search.c
+++ examples/controller/uecho_search/uecho_search.c
@@ -8,6 +8,7 @@
  *
  ******************************************************************/
 
+#include <stdio.h>
 #include <uecho/uecho.h>
 
 const int UECHO_TEST_SEARCH_WAIT_MTIME = 5000;
@@ -18,6 +19,18 @@ void uecho_search_messagelistener(uEchoController *ctrl, uEchoMessage *msg)
 
 void uecho_search_printdevices(uEchoController *ctrl)
 {
+  uEchoNode *node;
+  uEchoObject *obj;
+  int nodeNo, objNo;
+  
+  nodeNo = 0;
+  for (node = uecho_controller_getnodes(ctrl); node; node = uecho_node_next(node)) {
+    printf("Node[%d] : %s\n", (nodeNo++), uecho_node_getaddress(node));
+    objNo = 0;
+    for (obj = uecho_node_getobjects(node); obj; obj = uecho_object_next(obj)) {
+      printf("Object[%d] : %X\n", (objNo++), uecho_object_getcode(obj));
+    }
+  }
 }
 
 int main(int argc, char *argv[])
