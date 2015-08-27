406e52ed52fae6c510f9cf90d44ffdc424d25419
Satoshi Konno <skonno@cybergarage.org>
2015-08-27 22:02:32 +0900

    * Updated controller functions.

---
+++ include/uecho/uecho.h
@@ -0,0 +1,20 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _UECHO_UECHO_H_
+#define _UECHO_UECHO_H_
+
+#include <uecho/node.h>
+#include <uecho/object.h>
+#include <uecho/controller.h>
+
+#include <uecho/util/timer.h>
+
+#endif /* _UECHO_UECHO_H_ */
