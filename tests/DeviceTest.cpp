9c19e728f6a38084e2ed63038517493862b60f85
Satoshi Konno <skonno@cybergarage.org>
2015-09-01 00:39:10 +0900

    * Fixed to compile normally using GCC 4.8.4 on Ubuntu 14.04.

--- tests/DeviceTest.cpp
+++ tests/DeviceTest.cpp
@@ -35,9 +35,9 @@ BOOST_AUTO_TEST_CASE(DeviceDefaultProperties)
   
   BOOST_CHECK_EQUAL(uecho_device_isoperatingstatus(obj), true);
   BOOST_CHECK_EQUAL(uecho_device_isfaultstatus(obj), false);
-  BOOST_CHECK_EQUAL(uecho_device_getinstallationlocation(obj), uEchoDeviceInstallationLocationUnknown);
-  BOOST_CHECK_EQUAL(uecho_device_getstandardversion(obj), uEchoDeviceDefaultVersionAppendix);
-  BOOST_CHECK_EQUAL(uecho_device_getmanufacturercode(obj), uEchoManufactureCodeUnknown);
+  BOOST_CHECK_EQUAL(uecho_device_getinstallationlocation(obj), (byte)uEchoDeviceInstallationLocationUnknown);
+  BOOST_CHECK_EQUAL(uecho_device_getstandardversion(obj), (char)uEchoDeviceDefaultVersionAppendix);
+  BOOST_CHECK_EQUAL(uecho_device_getmanufacturercode(obj), (uEchoManufacturerCode)uEchoManufactureCodeUnknown);
   
   uecho_object_delete(obj);
 }
