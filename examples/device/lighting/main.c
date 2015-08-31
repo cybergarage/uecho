329e4f23a90e74caaceb9e736be4c2201c302341
Satoshi Konno <skonno@cybergarage.org>
2015-08-31 18:21:21 +0900

    * Updated examples.

--- examples/device/lighting/main.c
+++ examples/device/lighting/main.c
@@ -8,13 +8,57 @@
  *
  ******************************************************************/
 
+#include <signal.h>
+
 #include "lighting_dev.h"
 
 int main(int argc, char *argv[])
 {
   uEchoNode *node;
+  uEchoObject *obj;
   
-  node = uecho_create_lighting_node();
+  node = uecho_node_new();
+  if (!node)
+    return EXIT_FAILURE;
+  
+  obj = uecho_create_lighting_deviceobject();
+  if (!obj) {
+    uecho_node_delete(node);
+    return EXIT_FAILURE;
+  }
+  
+  uecho_node_addobject(node, obj);
+
+  if (!uecho_node_start(node)) {
+    return EXIT_FAILURE;
+  }
+  
+  while (uecho_node_isrunning(node)) {
+    sigset_t sigSet;
+    if (sigfillset(&sigSet) != 0)
+      break;
+    
+    int sigNo;
+    if (sigwait(&sigSet, &sigNo) != 0)
+      break;
+    
+    switch (sigNo) {
+      case SIGTERM:
+      case SIGINT:
+      case SIGKILL:
+        {
+          uecho_node_stop(node);
+        }
+        break;
+      case SIGHUP:
+        {
+          uecho_node_stop(node);
+          uecho_node_start(node);
+        }
+        break;
+    }
+  }
+  
   uecho_node_delete(node);
   
   return EXIT_SUCCESS;
