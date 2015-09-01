8f6ac0b89de69366c6fdbc27c9419664ad87b2f7
Satoshi Konno <skonno@cybergarage.org>
2015-09-01 23:37:12 +0900

    * Added tests for interface functions.

---
+++ tests/InterfaceTest.cpp
@@ -0,0 +1,20 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include <uecho/net/interface.h>
+
+BOOST_AUTO_TEST_CASE(GetInterface)
+{
+  uEchoNetworkInterface *netIfList = uecho_net_interfacelist_new();
+  BOOST_CHECK(0 < uecho_net_gethostinterfaces(netIfList));
+  uecho_net_interfacelist_delete(netIfList);
+}
