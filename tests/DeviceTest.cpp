ca06c21ae678cbb741a9deb7cac450722b47b7d5
Satoshi Konno <skonno@cybergarage.org>
2015-09-01 22:47:48 +0900

    * Fixed uecho_message_isnotify*().

--- tests/DeviceTest.cpp
+++ tests/DeviceTest.cpp
@@ -43,3 +43,47 @@ BOOST_AUTO_TEST_CASE(DeviceDefaultProperties)
   
   uecho_object_delete(obj);
 }
+
+BOOST_AUTO_TEST_CASE(DeviceRequest)
+{
+  // Create Controller (Disable UDP Server)
+  
+  uEchoController *ctrl = uecho_controller_new();
+  uecho_controller_disableudpserver(ctrl);
+  BOOST_CHECK(uecho_controller_start(ctrl));
+  
+  // Start Device
+  
+  uEchoNode *node = uecho_test_createtestnode();
+  BOOST_CHECK(uecho_node_start(node));
+  
+  // Search (NotificationRequest instead of ReadRequest)
+  
+  BOOST_CHECK(uecho_controller_searchallobjectswithesv(ctrl, uEchoEsvNotificationRequest));
+  
+  // Find device
+
+  uEchoObject *foundObj = uecho_controller_getobjectbycodewithwait(ctrl, UECHO_TEST_OBJECTCODE, UECHO_TEST_RESPONSE_WAIT_MAX_MTIME);
+  BOOST_CHECK(foundObj);
+  
+  // Send Message (NotificationRequest instead of ReadRequest)
+   
+  uEchoMessage *msg = uecho_message_new();
+  uecho_message_setesv(msg, uEchoEsvNotificationRequest);
+  BOOST_CHECK(uecho_message_setproperty(msg, UECHO_TEST_PROPERTY_SWITCHCODE, 0, NULL));
+  BOOST_CHECK(uecho_controller_sendmessage(ctrl, foundObj, msg));
+  uecho_message_delete(msg);
+   
+  // Check Response
+   
+  uEchoProperty *foundProp = uecho_object_getpropertywait(foundObj, UECHO_TEST_PROPERTY_SWITCHCODE, UECHO_TEST_RESPONSE_WAIT_MAX_MTIME);
+  BOOST_CHECK(foundProp);
+  
+  // Teminate
+  
+  BOOST_CHECK(uecho_controller_stop(ctrl));
+  uecho_controller_delete(ctrl);
+  
+  BOOST_CHECK(uecho_node_stop(node));
+  uecho_node_delete(node);
+}
