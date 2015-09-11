f67b491ac69c58cd5d6c7dcaeda464193b5ae97b
Satoshi Konno <skonno@cybergarage.org>
2015-09-11 20:55:11 +0900

    * Updated test cases for servers.

--- tests/ServerTest.cpp
+++ tests/ServerTest.cpp
@@ -10,6 +10,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include <uecho/core/server.h>
+#include <uecho/net/interface.h>
 
 BOOST_AUTO_TEST_CASE(ServerTest)
 {
@@ -25,6 +26,58 @@ BOOST_AUTO_TEST_CASE(ServerTest)
 
 BOOST_AUTO_TEST_CASE(UdpServerTest)
 {
+  uEchoNetworkInterfaceList *netIfList = uecho_net_interfacelist_new();
+  
+  BOOST_CHECK(0 < uecho_net_gethostinterfaces(netIfList));
+  
+  if (uecho_net_gethostinterfaces(netIfList) <= 0) {
+    uecho_net_interfacelist_delete(netIfList);
+    return;
+  }
+  
+  uEchoNetworkInterface *netIf = uecho_net_interfacelist_gets(netIfList);
+  const char *bindAddr = uecho_net_interface_getaddress(netIf);
+  
+  uEchoUdpServer *server = uecho_udp_server_new();
+  BOOST_CHECK(server);
+  
+  BOOST_CHECK(uecho_udp_server_open(server, bindAddr));
+  BOOST_CHECK(uecho_udp_server_start(server));
+  BOOST_CHECK(uecho_udp_server_stop(server));
+  
+  BOOST_CHECK(uecho_udp_server_delete(server));
+  
+  uecho_net_interfacelist_delete(netIfList);
+}
+
+BOOST_AUTO_TEST_CASE(McastServerTest)
+{
+  uEchoNetworkInterfaceList *netIfList = uecho_net_interfacelist_new();
+  
+  BOOST_CHECK(0 < uecho_net_gethostinterfaces(netIfList));
+  
+  if (uecho_net_gethostinterfaces(netIfList) <= 0) {
+    uecho_net_interfacelist_delete(netIfList);
+    return;
+  }
+  
+  uEchoNetworkInterface *netIf = uecho_net_interfacelist_gets(netIfList);
+  const char *bindAddr = uecho_net_interface_getaddress(netIf);
+  
+  uEchoMcastServer *server = uecho_mcast_server_new();
+  BOOST_CHECK(server);
+  
+  BOOST_CHECK(uecho_mcast_server_open(server, bindAddr));
+  BOOST_CHECK(uecho_mcast_server_start(server));
+  BOOST_CHECK(uecho_mcast_server_stop(server));
+  
+  BOOST_CHECK(uecho_mcast_server_delete(server));
+  
+  uecho_net_interfacelist_delete(netIfList);
+}
+
+BOOST_AUTO_TEST_CASE(UdpServerListTest)
+{
   uEchoUdpServerList *servers = uecho_udp_serverlist_new();
   
   BOOST_CHECK(servers);
@@ -36,7 +89,7 @@ BOOST_AUTO_TEST_CASE(UdpServerTest)
   uecho_udp_serverlist_delete(servers);
 }
 
-BOOST_AUTO_TEST_CASE(McastServerTest)
+BOOST_AUTO_TEST_CASE(McastServerListTest)
 {
   uEchoMcastServerList *servers = uecho_mcast_serverlist_new();
   
@@ -48,3 +101,4 @@ BOOST_AUTO_TEST_CASE(McastServerTest)
   
   uecho_mcast_serverlist_delete(servers);
 }
+
