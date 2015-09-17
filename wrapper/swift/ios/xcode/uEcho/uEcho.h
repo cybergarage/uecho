8021099eece06a38530401d90e909cd409ca5203
Satoshi Konno <skonno@cybergarage.org>
2015-09-18 03:48:56 +0900

    * Added a skelton framework project for Swift.

---
+++ wrapper/swift/ios/xcode/uEcho/uEcho.h
@@ -0,0 +1,20 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for uEcho.
+FOUNDATION_EXPORT double uEchoVersionNumber;
+
+//! Project version string for uEcho.
+FOUNDATION_EXPORT const unsigned char uEchoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uEcho/PublicHeader.h>
+#import <uecho/uecho.h>
---
+++ wrapper/swift/uEchoNode.swift
@@ -0,0 +1,17 @@
+/******************************************************************
+*
+* uEcho for Swift
+*
+* Copyright (C) The uecho Authors 2015
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+import uEchoC
+
+public class uEchoNode {
+  
+  var cobj: UnsafeMutablePointer<Void>? = nil
+}
+
