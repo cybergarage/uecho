f8b253a423b2e04266ba9d641751aa951aa44af2
Satoshi Konno <skonno@cybergarage.org>
2015-09-18 03:48:56 +0900

    * Added a skelton framework project for Swift.

---
+++ wrapper/swift/uEchoProperty.swift
@@ -0,0 +1,18 @@
+/******************************************************************
+*
+* uEcho for Swift
+*
+* Copyright (C) The uecho Authors 2015
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+import uEchoC
+
+public class uEchoProperty {
+  
+  var cobj: UnsafeMutablePointer<Void>? = nil
+  
+}
+
