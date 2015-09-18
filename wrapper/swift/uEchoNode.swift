45362080eab346944d7dfba53ea1ca54be01fafe
Satoshi Konno <skonno@cybergarage.org>
2015-09-18 09:48:19 +0900

    * Added uEchoObject::getProperties().

--- wrapper/swift/uEchoNode.swift
+++ wrapper/swift/uEchoNode.swift
@@ -11,7 +11,23 @@
 import uEchoC
 
 public class uEchoNode {
+
   var cobj: UnsafeMutablePointer<Void>? = nil
 
+  public init() {
+  }
+
+  public func getObjects() -> [uEchoObject] {
+    var objs = [uEchoObject]()
+    if self.cobj != nil {
+      return objs
+    }
+    for var co = uecho_node_getobjects(self.cobj!); co != nil; co = uecho_object_next(co) {
+      let sobj = uEchoObject()
+      sobj.cobj = co
+      objs.append(sobj)
+    }
+    return objs
+  }
 }
 
