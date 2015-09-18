b11973b8cdb7553772f107f50706818a8deafc7b
Satoshi Konno <skonno@cybergarage.org>
2015-09-18 23:30:55 +0900

    * Added uEchoMessage.

---
+++ wrapper/swift/uEchoMessage.swift
@@ -0,0 +1,24 @@
+/******************************************************************
+*
+* uEcho for Swift
+*
+* Copyright (C) Satoshi Konno 2015
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+import uEchoC
+
+public class uEchoMessage {
+  
+  var cobj: UnsafeMutablePointer<Void>? = nil
+
+  public init() {
+    self.cobj = uecho_message_new();
+  }
+
+  init(cobj: UnsafeMutablePointer<Void>?) {
+    self.cobj = cobj
+  }
+}
