cb2090680b224cc83a70b1f97da26b04f869bb18
Satoshi Konno <skonno@cybergarage.org>
2015-09-18 09:48:19 +0900

    * Added uEchoObject::getProperties().

--- wrapper/swift/uEchoObject.swift
+++ wrapper/swift/uEchoObject.swift
@@ -11,6 +11,23 @@
 import uEchoC
 
 public class uEchoObject {
+  
   var cobj: UnsafeMutablePointer<Void>? = nil
+
+  public init() {
+  }
+  
+  public func getProperties() -> [uEchoProperty] {
+    var props = [uEchoProperty]()
+    if self.cobj != nil {
+      return props
+    }
+    for var cprop = uecho_object_getproperties(self.cobj!); cprop != nil; cprop = uecho_property_next(cprop) {
+      let prop = uEchoProperty()
+      prop.cobj = cprop
+      props.append(prop)
+    }
+    return props
+  }
 }
 
