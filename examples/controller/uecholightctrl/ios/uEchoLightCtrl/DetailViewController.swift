c27117c3c24acb1561b2669ab1aaad60b1046ad1
Satoshi Konno <skonno@cybergarage.org>
2015-10-04 00:50:01 +0900

    * Updated sample projects.

--- examples/controller/uecholightctrl/ios/uEchoLightCtrl/DetailViewController.swift
+++ examples/controller/uecholightctrl/ios/uEchoLightCtrl/DetailViewController.swift
@@ -14,16 +14,13 @@ class DetailViewController: UIViewController {
 
   @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
   var detailItem: AnyObject? {
     didSet {
-        // Update the view.
         self.configureView()
     }
   }
 
   func configureView() {
-    // Update the user interface for the detail item.
     if let detail = self.detailItem {
         if let label = self.detailDescriptionLabel {
             label.text = detail.description
@@ -33,15 +30,11 @@ class DetailViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     self.configureView()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
 }
 
