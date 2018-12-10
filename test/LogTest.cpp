401dccd041d2d0a5da8f4e326eee4e65c665d505
Satoshi Konno <skonno@cybergarage.org>
2018-12-11 00:30:15 +0900

    Add uecho_log_*() for debug

---
+++ test/LogTest.cpp
@@ -0,0 +1,23 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include <uecho/util/log.h>
+
+BOOST_AUTO_TEST_CASE(LogFunction)
+{
+  const char *msg = "hello";
+  
+  uecho_log_debug("%s", msg);
+  uecho_log_warn("%s", msg);
+  uecho_log_error("%s", msg);
+  uecho_log_info("%s", msg);
+}
