6ea050764d1e30abff6c54fdb288621a9ac44257
Satoshi Konno <skonno@cybergarage.org>
2022-02-26 00:25:05 +0900

    Add uecho_message_debug()

---
+++ src/uecho/logs.h
@@ -0,0 +1,18 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _UECHO_LOG_FUNC_H_
+#define _UECHO_LOG_FUNC_H_
+
+#include <uecho/message_internal.h>
+
+void uecho_message_debug(uEchoMessage* msg);
+
+#endif /* _UECHO_LOG_FUNC_H_ */
