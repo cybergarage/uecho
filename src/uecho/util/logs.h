cb6ac204b84ba8202744cdbe6682972b5c800849
Satoshi Konno <skonno@cybergarage.org>
2022-02-26 14:59:12 +0900

    Update uecho_net_packet_debug()

--- src/uecho/util/logs.h
+++ src/uecho/util/logs.h
@@ -13,8 +13,8 @@
 
 #include <uecho/message_internal.h>
 
-void uecho_message_packet_debug(const char* addr, const byte* msg, size_t msg_len);
-void uecho_socket_datagram_packet_debug(uEchoDatagramPacket* dpkt);
 void uecho_message_debug(uEchoMessage* msg);
+void uecho_net_datagram_packet_debug(uEchoDatagramPacket* dpkt);
+void uecho_net_packet_debug(const char* from_addr, const char* to_addr, const byte* msg_bytes, size_t msg_len);
 
 #endif /* _UECHO_LOG_FUNC_H_ */
