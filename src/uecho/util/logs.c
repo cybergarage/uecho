5ca4f8ce2a47d6d63c0252f22fb9f04e81f981a1
Satoshi Konno <skonno@cybergarage.org>
2022-03-03 00:25:18 +0900

    Add uecho_net_packet_log()

--- src/uecho/util/logs.c
+++ src/uecho/util/logs.c
@@ -14,6 +14,43 @@
 #include <uecho/util/logs.h>
 
 /****************************************
+* uecho_net_packet_log
+****************************************/
+
+void uecho_net_packet_log(int severity, const char *prefix, const char* from_addr, const char* to_addr, const byte* msg_bytes, size_t msg_len)
+{
+  char buf[256];
+  size_t offset;
+  size_t n;
+
+  if (msg_len <= 0)
+    return;
+
+  offset = 0;
+  if (prefix && (0 < strlen(prefix))) {
+    snprintf(buf, sizeof(buf), "%s ", prefix);
+    offset = strlen(buf);
+  }
+  snprintf((buf + offset), (sizeof(buf) - offset), "%-15s -> %-15s ", from_addr, to_addr);
+  offset = strlen(buf);
+  for (n = 0; n < msg_len; n++) {
+    snprintf((buf + offset), (sizeof(buf) - offset), "%02X", msg_bytes[n]);
+    offset += 2;
+  }
+
+  uecho_log_debug(buf);
+}
+
+/****************************************
+* uecho_net_packet_log
+****************************************/
+
+void uecho_net_packet_debug(const char*prefix, const char* from_addr, const char* to_addr, const byte* pkt_bytes, size_t pkt_len)
+{
+  return uecho_net_packet_log(LOG_ERROR, prefix, from_addr,to_addr, pkt_bytes, pkt_len);
+}
+
+/****************************************
 * uecho_message_debug
 ****************************************/
 
@@ -47,30 +84,3 @@ void uecho_net_datagram_packet_debug(uEchoDatagramPacket* dpkt)
       uecho_socket_datagram_packet_getlength(dpkt));
 }
 
-/****************************************
-* uecho_net_packet_debug
-****************************************/
-
-void uecho_net_packet_debug(const char *prefix, const char* from_addr, const char* to_addr, const byte* msg_bytes, size_t msg_len)
-{
-  char buf[256];
-  size_t offset;
-  size_t n;
-
-  if (msg_len <= 0)
-    return;
-
-  offset = 0;
-  if (prefix && (0 < strlen(prefix))) {
-    snprintf(buf, sizeof(buf), "%s ", prefix);
-    offset = strlen(buf);
-  }
-  snprintf((buf + offset), (sizeof(buf) - offset), "%-15s -> %-15s ", from_addr, to_addr);
-  offset = strlen(buf);
-  for (n = 0; n < msg_len; n++) {
-    snprintf((buf + offset), (sizeof(buf) - offset), "%02X", msg_bytes[n]);
-    offset += 2;
-  }
-
-  uecho_log_debug(buf);
-}
