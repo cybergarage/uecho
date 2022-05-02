f84be817b2a69302a9b647cbc37d7fb245018d4c
Satoshi Konno <skonno@cybergarage.org>
2022-05-02 19:47:36 +0900

    Add XCode projects for Objective-C

---
+++ wrapper/objc/xcode/uEcho4ObjC/uEcho.h
@@ -0,0 +1,18 @@
+//
+//  uecho.h
+//  uecho
+//
+//  Created by The uecho Authors on 2022/05/02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for uecho.
+FOUNDATION_EXPORT double uechoVersionNumber;
+
+//! Project version string for uecho.
+FOUNDATION_EXPORT const unsigned char uechoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uecho/PublicHeader.h>
+
+
