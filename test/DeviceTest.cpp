0bb17977bb4fbb893444b448b9bcbb9a36f95191
Satoshi Konno <skonno@cybergarage.org>
2022-05-02 14:55:45 +0900

    Update DeviceRequest()

--- test/DeviceTest.cpp
+++ test/DeviceTest.cpp
@@ -109,10 +109,10 @@ BOOST_AUTO_TEST_CASE(DeviceRequest)
   BOOST_CHECK(uecho_controller_sendmessage(ctrl, found_obj, msg));
   uecho_message_delete(msg);
 
-  // Send Message (NotificationRequest instead of ReadRequest)
+  // Send Message (ReadRequest)
 
   msg = uecho_message_new();
-  uecho_message_setesv(msg, uEchoEsvNotificationRequest);
+  uecho_message_setesv(msg, uEchoEsvReadRequest);
   BOOST_CHECK(uecho_message_setproperty(msg, UECHO_TEST_PROPERTY_SWITCHCODE, 0, NULL));
   BOOST_CHECK(uecho_controller_sendmessage(ctrl, found_obj, msg));
   uecho_message_delete(msg);
