0e4147567d5e4986d98e4c9a5c7d50521becdd94
Satoshi Konno <skonno@cybergarage.org>
2022-05-03 23:00:51 +0900

    Add CGEchoNode

--- wrapper/objc/uEcho/CGEcho.h
+++ wrapper/objc/uEcho/CGEcho.h
@@ -14,4 +14,4 @@ FOUNDATION_EXPORT const unsigned char uechoVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <uecho/PublicHeader.h>
 
-#import "CGEchoController.h"
+#import <CGEcho/CGEchoController.h>
