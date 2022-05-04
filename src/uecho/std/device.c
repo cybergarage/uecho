728afeaee1e52698d719513c90b46dee3a0b8399
Satoshi Konno <skonno@cybergarage.org>
2022-05-04 23:03:48 +0900

    Update uecho_device_addmandatoryproperties()

--- src/uecho/std/device.c
+++ src/uecho/std/device.c
@@ -23,7 +23,10 @@ uEchoObject* uecho_device_new(void)
   if (!obj)
     return NULL;
 
-  uecho_device_addmandatoryproperties(obj);
+  if (!uecho_device_addmandatoryproperties(obj)) {
+    uecho_object_delete(obj);
+    return NULL;
+  }
 
   return obj;
 }
