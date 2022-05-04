8a6cb8857e71ff26b8884a8945451b6d4e0da123
Satoshi Konno <skonno@cybergarage.org>
2022-05-04 23:16:58 +0900

    Update uecho_nodeprofile_addmandatoryproperties()

--- src/uecho/std/node_profile_class.c
+++ src/uecho/std/node_profile_class.c
@@ -58,7 +58,13 @@ bool uecho_nodeprofile_addmandatoryproperties(uEchoObject* obj)
   if (!super_obj)
     return false;
 
-  return uecho_object_copyobjectproperties(obj, super_obj);
+  if (!uecho_object_copyobjectproperties(obj, super_obj))
+    return false;
+  
+  if (!uecho_nodeprofile_setoperatingstatus(obj, true))
+    return false;
+
+  return true;
 }
 
 /****************************************
