4bdb1d88de0f26f447f72b39f1469366a0158197
cybergarage <cybergarage@users.noreply.github.com>
2022-05-05 10:26:26 +0000

    deploy: 22ae8a6db84e1697161419fa1c305af925354c7e

--- globals_dup.js
+++ globals_dup.js
@@ -1,8 +1,8 @@
 var globals_dup =
 [
-    [ "_", "globals.html", null ],
-    [ "b", "globals_b.html", null ],
+    [ "b", "globals.html", null ],
     [ "f", "globals_f.html", null ],
+    [ "g", "globals_g.html", null ],
     [ "l", "globals_l.html", null ],
     [ "m", "globals_m.html", null ],
     [ "p", "globals_p.html", null ],
