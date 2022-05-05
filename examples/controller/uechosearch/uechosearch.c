4289deafe4300e71c094b5b463624ab67d154ba4
Satoshi Konno <skonno@cybergarage.org>
2022-05-06 01:47:57 +0900

    Update uechosearch to output mandatory property data in verbose mode

--- examples/controller/uechosearch/uechosearch.c
+++ examples/controller/uechosearch/uechosearch.c
@@ -13,7 +13,7 @@
 
 #include <uecho/uecho.h>
 
-const int UECHO_TEST_SEARCH_WAIT_MTIME = 5000;
+const int UECHO_TEST_SEARCH_WAIT_MTIME = 2000;
 
 void usage()
 {
@@ -22,27 +22,81 @@ void usage()
   printf(" -h : Print this message\n");
 }
 
-uEchoMessage *create_readmessage(uEchoController* ctrl, byte prop_code)
+uEchoMessage *create_readpropertymessagebycode(int obj_code, byte prop_code)
 {
   uEchoMessage* msg;
   msg = uecho_message_new();
   uecho_message_setesv(msg, uEchoEsvReadRequest);
+  uecho_message_setdestinationobjectcode(msg, obj_code);
   uecho_message_setproperty(msg, prop_code, NULL, 0);
   return msg;
+}
+
+uEchoMessage *create_readpropertymessage(uEchoProperty* prop)
+{
+  return create_readpropertymessagebycode(
+    uecho_object_getcode(uecho_property_getparentobject(prop)),
+    uecho_property_getcode(prop)
+  );
 }
 
 void print_founddevices(uEchoController* ctrl, bool verbose)
 {
   uEchoNode* node;
   uEchoObject* obj;
+  uEchoProperty* prop;
+  uEchoProperty* res_prop;
+  uEchoMessage* req_msg;
+  uEchoMessage* res_msg;
+  int obj_no;
+  int prop_no;
+  int res_prop_no;
+  int n;
+  byte *res_prop_bytes;
+  const char *manufacture_name;
 
   for (node = uecho_controller_getnodes(ctrl); node; node = uecho_node_next(node)) {
-    if (uecho_node_getobjectcount(node) <= 0) {
-      printf("%s\n", uecho_node_getaddress(node));
+    printf("%-15s ", uecho_node_getaddress(node));
+
+    if (!verbose) {
+      obj_no = 0;
+      for (obj = uecho_node_getobjects(node); obj; obj = uecho_object_next(obj)) {
+        printf("[%d] %06X ", obj_no, uecho_object_getcode(obj));
+        obj_no++;
+      }
+      printf("\n");
       continue;
     }
+
+    printf("\n");
+    obj_no = 0;
     for (obj = uecho_node_getobjects(node); obj; obj = uecho_object_next(obj)) {
-      printf("%s %06X\n", uecho_node_getaddress(node), uecho_object_getcode(obj));
+      printf("[%d] %06X (%s)\n", obj_no, uecho_object_getcode(obj), uecho_object_getname(obj));
+
+      prop_no = 0;
+      for (prop = uecho_object_getproperties(obj); prop; prop = uecho_property_next(prop)) {
+        if (uecho_property_isreadrequired(prop)) {
+          req_msg = create_readpropertymessage(prop);
+          res_msg = uecho_message_new();
+          printf("[%d] [%d] %02X (%s) ", obj_no, prop_no, uecho_property_getcode(prop), uecho_property_getname(prop));
+          if (uecho_controller_postmessage(ctrl, node, req_msg, res_msg)) {
+            for (res_prop_no=0; res_prop_no<uecho_message_getopc(res_msg); res_prop_no++) {
+              res_prop = uecho_message_getproperty(res_msg, res_prop_no);
+              if (!res_prop)
+                continue;
+              res_prop_bytes = uecho_property_getdata(res_prop);
+              for (n=0; n<uecho_property_getdatasize(res_prop); n++) {
+                printf("%02X", res_prop_bytes[n]);
+              }
+            }
+          }
+          printf("\n");
+          uecho_message_delete(req_msg);
+          uecho_message_delete(res_msg);
+        }
+        prop_no++;
+      }
+      obj_no++;
     }
   }
 }
