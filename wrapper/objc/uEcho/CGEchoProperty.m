52e22eee9a0dd630c22aa7766a62c4121d087025
Satoshi Konno <skonno@cybergarage.org>
2022-05-07 02:01:28 +0900

    Add CGEchoProperty

---
+++ wrapper/objc/uEcho/CGEchoProperty.m
@@ -0,0 +1,31 @@
+/******************************************************************
+ *
+ * uEcho for ObjC
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <uecho/property.h>
+
+#import "CGEchoProperty.h"
+
+@implementation CGEchoProperty
+
+@synthesize cObject;
+
+- (id)initWithCObject:(uEchoProperty*)cobj
+{
+  if ((self = [super init]) == nil)
+    return nil;
+  cObject = cobj;
+  return self;
+}
+
+- (void)dealloc
+{
+}
+
+@end
