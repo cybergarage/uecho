b9f9cac10c4802c7604474131e276748ddbe5212
Satoshi Konno <skonno@cybergarage.org>
2022-05-07 02:06:57 +0900

    Add CGEchoMessage

---
+++ wrapper/objc/uEcho/CGEchoMessage.h
@@ -0,0 +1,20 @@
+/******************************************************************
+ *
+ * uEcho for ObjC
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#import <Foundation/NSArray.h>
+#import <Foundation/NSString.h>
+
+#include <uecho/message.h>
+
+@interface CGEchoMessage : NSObject {
+}
+@property (assign) uEchoMessage* cObject;
+- (id)initWithCObject:(uEchoMessage*)cobj;
+@end
