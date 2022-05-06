5d885fa5be464284f259324edcea58c5cda86213
Satoshi Konno <skonno@cybergarage.org>
2022-05-07 01:37:12 +0900

    Add CGEchoObject

---
+++ wrapper/objc/uEcho/CGEchoObject.m
@@ -0,0 +1,31 @@
+/******************************************************************
+ *
+ * uEcho for ObjC
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <uecho/object.h>
+
+#import "CGEchoObject.h"
+
+@implementation CGEchoObject
+
+@synthesize cObject;
+
+- (id)initWithCObject:(uEchoObject*)cobj
+{
+  if ((self = [super init]) == nil)
+    return nil;
+  cObject = cobj;
+  return self;
+}
+
+- (void)dealloc
+{
+}
+
+@end
