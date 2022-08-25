55c93a023ab6efe76f4c81c194aeb0ee894cab5b
Satoshi Konno <skonno@cybergarage.org>
2022-08-26 01:03:49 +0900

    Add PropertyMapTest

---
+++ test/PropertyMapTest.cpp
@@ -0,0 +1,42 @@
+/******************************************************************
+ *
+ * uEcho for C
+ *
+ * Copyright (C) The uecho Authors 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <vector>
+
+#include <uecho/device.h>
+
+BOOST_AUTO_TEST_CASE(PropertyMap)
+{
+  uEchoObjectCode obj_codes[] = {
+    0x03CE,
+  };
+  
+  for (int i=0; i < sizeof(obj_codes)/sizeof(obj_codes[0]); i++) {
+    uEchoObjectCode obj_code = obj_codes[i];
+    uEchoObject *obj = uecho_device_new();
+    uecho_object_setcode(obj, obj_code);
+
+    uEchoPropertyCode prop_codes[] = {
+      uEchoObjectGetPropertyMap,
+      uEchoObjectSetPropertyMap,
+      uEchoObjectAnnoPropertyMap,
+    };
+
+    for (int j=0; j < sizeof(prop_codes)/sizeof(prop_codes[0]); j++) {
+      uEchoPropertyCode prop_code = prop_codes[i];
+      uEchoProperty *prop = uecho_object_getproperty(obj, prop_code);
+      BOOST_CHECK(prop);
+    }
+    
+    uecho_object_delete(obj);
+  }
+}
