aeeae7f0a66710d088b3f7eb668b98bf50e63dc9
Satoshi Konno <skonno@cybergarage.org>
2023-01-06 08:47:15 +0900

    Update uecho_cond_*()

--- src/uecho/util/cond.c
+++ src/uecho/util/cond.c
@@ -57,7 +57,9 @@ bool uecho_cond_wait(uEchoCond* cond)
   if (!cond)
     return false;
 
+  pthread_mutex_lock(&cond->mutex_id);
   pthread_cond_wait(&cond->cond_id, &cond->mutex_id);
+  pthread_mutex_unlock(&cond->mutex_id);
 
   return true;
 }
@@ -66,14 +68,22 @@ bool uecho_cond_wait(uEchoCond* cond)
  * uecho_cond_timedwait
  ****************************************/
 
-bool uecho_cond_timedwait(uEchoCond* cond)
+bool uecho_cond_timedwait(uEchoCond* cond, clock_t mtime)
 {
+  struct timespec to;
+  int c;
+
   if (!cond)
     return false;
 
-  pthread_cond_wait(&cond->cond_id, &cond->mutex_id);
+  to.tv_sec = time(NULL) + (mtime / CLOCKS_PER_SEC);
+  to.tv_nsec = 0;
 
-  return true;
+  pthread_mutex_lock(&cond->mutex_id);
+  c = pthread_cond_timedwait(&cond->cond_id, &cond->mutex_id, &to);
+  pthread_mutex_unlock(&cond->mutex_id);
+
+  return (c == 0) ? true : false;
 }
 
 /****************************************
@@ -85,7 +95,9 @@ bool uecho_cond_signal(uEchoCond* cond)
   if (!cond)
     return false;
 
+  pthread_mutex_lock(&cond->mutex_id);
   pthread_cond_signal(&cond->cond_id);
+  pthread_mutex_unlock(&cond->mutex_id);
 
   return true;
 }
