eed7e556f2b254bffe84bd83fde6d3a7adc117eb
Satoshi Konno <skonno@cybergarage.org>
2023-01-06 09:11:41 +0900

    Update uecho_controller_postmessage()

--- include/uecho/controller.h
+++ include/uecho/controller.h
@@ -23,7 +23,7 @@ extern "C" {
  * Constant
  ****************************************/
 
-#define uEchoControllerPostResponseMaxMiliTime 5000
+#define uEchoControllerPostResponseMaxMiliTime (5 * CLOCKS_PER_SEC)
 
 /****************************************
  * Data Type
